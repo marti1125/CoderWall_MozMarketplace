4a1a722f94f8411da0f6d69073bc842ee18fdaa2
Willy Aguirre <marti1125@gmail.com>
2013-01-09 09:40:10 -0500

    change design

--- js/app.js
+++ js/app.js
@@ -1,36 +1,84 @@
-$(document).ready(function() {	
-	
-	$('#btnSearch').click(function(){
+$(document).ready(function () {
+
+    $('#btnSearch').click(function () {
+
+        $('#result').html('');
+        $('#bimg').html('');
+        $('#btext').html('');
+
+        var search = $("#username").val();
+
+        $.ajax({
+            url: 'https://coderwall.com/' + search + '.json?callback=?',
+            dataType: "json",
+            timeout: 5000,
+            success: function (data) {
+
+                $.each(data, function (key, val) {
+
+                    var username = val.username;
+                    var name = val.name;
+                    var location = val.location;
+                    var github = val.accounts.github;
+                    $('#result').append('<h1>' + name + '</h1>');
+                    $('#result').append('<h2>' + location + '</h2>');
+                    $('#result').append('<p>Github account: <a href=https://github.com/' + github + ' target=_blank>' + github + '</a></p>');
+
+                    $.each(val.badges, function (key2, val2) {
+                        var badge_name = val2.name;
+                        var badge_desp = val2.description;
+                        var badge_img = val2.badge;
+                        $('#bimg').append('<li class="badge"><figure><img src=' + badge_img + '/><figcaption><p>' + badge_desp + '</p><b>' + badge_name + '</b></figcaption></figure></li>');                       
+                    });
+
+                });
+            },
+            error: function (data) {
+                alert('Username does not found');
+            }
+        });
+
+    });
+
+	$('div#form form').submit(function () {
 
 		$('#result').html('');
-		$('#bimg').html('');
-		$('#btext').html('');
+        $('#bimg').html('');
+        $('#btext').html('');
 
-		var search = $("#username").val();
+        var search = $("#username").val();
 
-		$.getJSON('https://coderwall.com/' + search + '.json?callback=?', function (data) {
-			$.each(data, function (key, val) {
+        $.ajax({
+            url: 'https://coderwall.com/' + search + '.json?callback=?',
+            dataType: "json",
+            timeout: 5000,
+            success: function (data) {
 
-				var username = val.username;
-				var name = val.name;
-				var location = val.location;
-				var github = val.accounts.github;
-				$('#result').append('<h1>'+name+'</h1>');
-				$('#result').append('<h2>'+location+'</h2>');								
-				$('#result').append('<p>Github account: <a href=https://github.com/'+github+' target=_blank>'+github+'</a></p>');
+                $.each(data, function (key, val) {
 
-				$.each(val.badges, function (key2, val2) {
-					var badge_name = val2.name;
-					var badge_desp = val2.description;
-					var badge_img = val2.badge;
-					$('#bimg').append('<img src='+badge_img+'/>');
-					$('#bimg').append('<p>'+badge_name+'</p>');
-				});	
+                    var username = val.username;
+                    var name = val.name;
+                    var location = val.location;
+                    var github = val.accounts.github;
+                    $('#result').append('<h1>' + name + '</h1>');
+                    $('#result').append('<h2>' + location + '</h2>');
+                    $('#result').append('<p>Github account: <a href=https://github.com/' + github + ' target=_blank>' + github + '</a></p>');
 
-			});
+                    $.each(val.badges, function (key2, val2) {
+                        var badge_name = val2.name;
+                        var badge_desp = val2.description;
+                        var badge_img = val2.badge;
+                        $('#bimg').append('<li class="badge"><figure><img src=' + badge_img + '/><figcaption><p>' + badge_desp + '</p><b>' + badge_name + '</b></figcaption></figure></li>');                       
 
-		});	
+                    });
 
-	});	
-	
+                });
+            },
+            error: function (data) {
+                alert('Username does not found');
+            }
+        });
+
+	});
+
 });
