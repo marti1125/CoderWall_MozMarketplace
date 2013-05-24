5e9fdeddea6422cdb8d7e14b5516fd73b3b36bef
Willy Aguirre <marti1003@hotmail.com>
2013-05-24 07:58:25 -0500

    se modifica el diseño

--- js/app.js
+++ js/app.js
@@ -1,61 +1,7 @@
-/*function addBadges(user){  
-
-    $('#result').html('');
-    $('#bimg').html('');
-    $('#btext').html('');
-
-    var search = $("#username").val();
-
-    $.ajax({
-        url: 'https://coderwall.com/' + search + '.json?callback=?',
-        dataType: "json",
-        timeout: 9000,        
-        success: function (data) {
-
-            $.each(data, function (key, val) {
-
-                var username = val.username;
-                var name = val.name;
-                var location = val.location;
-                var github = val.accounts.github;
-                $('#result').append('<h1>' + name + '</h1>');
-                $('#result').append('<h2>' + location + '</h2>');
-                $('#result').append('<p>Github account: <a href=https://github.com/' + github + ' target=_blank>' + github + '</a></p>');
-
-                $.each(val.badges, function (key2, val2) {
-                    var badge_name = val2.name;
-                    var badge_desp = val2.description;
-                    var badge_img = val2.badge;
-                    $('#bimg').append('<li class="bassdge"><figure><img src=' + badge_img + '/><figcaption><p id=despt >' + badge_desp + '</p><b class=name>' + badge_name + '</b></figcaption></figure></li>');                       
-
-                });
-
-            });
-        },
-        error: function (data) {
-            alert('Username does not found');
-        }
-    });
-
-}
-
-
-$(document).ready(function () {
-
-    $('#btnSearch').click(function (e) {
-
-        e.preventDefault();
-
-        addBadges();        
-
-    });
-
+$('#reset').click(function(){	
+	$('#username').html('');
 });
 
-*/
-
-
-//Show / Hide Settings view
 var btnSettings = document.querySelector("#settings-btn");
 
 var viewSettings = document.querySelector("#settings-view");
