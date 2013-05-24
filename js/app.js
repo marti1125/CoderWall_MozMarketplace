55fae7fc1ac3a9abccf8fc117c0e42951e64a520
Willy Aguirre <marti1003@hotmail.com>
2013-05-23 23:01:38 -0500

    se agrego el nuevo estilo

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-function addBadges(user){  
+/*function addBadges(user){  
 
     $('#result').html('');
     $('#bimg').html('');
@@ -50,6 +50,25 @@ $(document).ready(function () {
 
     });
 
-	
-
 });
+
+*/
+
+
+//Show / Hide Settings view
+var btnSettings = document.querySelector("#settings-btn");
+
+var viewSettings = document.querySelector("#settings-view");
+
+btnSettings.addEventListener ('click', function () {
+	viewSettings.classList.remove('move-down');
+	viewSettings.classList.add('move-up');
+});
+
+var btnCloseSettings = document.querySelector("#close-btn");
+
+btnCloseSettings.addEventListener ('click', function () {
+	viewSettings.classList.remove('move-up');
+	viewSettings.classList.add('move-down');
+});
+
