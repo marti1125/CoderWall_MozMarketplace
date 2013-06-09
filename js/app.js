18f4c631f2eb9408bdabf3fdb79e1f8f1ae2bb8c
Willy Aguirre <marti1003@hotmail.com>
2013-06-09 09:39:36 -0500

    se retira un boton innecesario

--- js/app.js
+++ js/app.js
@@ -6,8 +6,3 @@ $("#settings-btn").click(function(){
 	$("#settings-view").removeClass('move-down');
 	$("#settings-view").addClass('move-up');
 });
-
-$("#close-btn").click(function() {
-	$("#settings-view").removeClass('move-up');
-	$("#settings-view").addClass('move-down');
-});
